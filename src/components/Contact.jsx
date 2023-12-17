8890b99033355c89873a641b957599d992d729a0
Asad Jamil <bulediasadjamil@gmail.com>
2023-12-17 12:22:39 +0500

    functionality: added email functionality to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,8 +12,39 @@ const Contact = () => {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [loading, setloading] = useState(false);
 
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setloading(true);
+    emailjs
+      .send(
+        "service_kjl9lyq",
+        "template_motl8k5",
+        {
+          from_name: form.name,
+          to_name: "Jamil Ahmed Buledi",
+          from_email: form.email,
+          to_email: "buledijamil@gmail.com",
+          message: form.message,
+        },
+        "T9DcCkHCRed1W10er"
+      )
+      .then(
+        () => {
+          setloading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+          setForm({ name: "", email: "", message: "" });
+        },
+        (error) => {
+          setloading(false);
+          console.log(error);
+          alert("Something went worng.");
+        }
+      );
+  };
 
   return (
     <div className="xl:m-t12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden capitalize">
