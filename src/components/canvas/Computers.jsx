d5605dcc99775810c0205064445132a17ed4ba77
Asad Jamil <bulediasadjamil@gmail.com>
2024-01-11 13:37:03 +0500

    update: computer rotate speed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -56,6 +56,7 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           autoRotate
+          autoRotateSpeed={0.1}
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
